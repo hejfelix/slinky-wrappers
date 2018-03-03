204d8cddcee31e2ba8af4cd4d23fae9600cb01cd
Felix Palludan Hargreaves <hejfelix@gmail.com>
2018-03-03 17:43:39 +0100

    Fix test

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -4,6 +4,6 @@ import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+    Hello.greeting shouldEqual "Hello"
   }
 }
